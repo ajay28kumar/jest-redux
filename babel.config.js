4c559fe2bd0935063d41cbfe74d148b346e13187
Ajay Kumar <ajaykumar@Ajays-MacBook-Air.local>
2019-10-05 16:12:30 +0530

    added preact 10x support

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,7 @@
 
-
 module.exports = {
-	presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-flow'],
 	plugins: [
-		// adding this one solved the issue
-		'@babel/plugin-transform-modules-commonjs',
-		'@babel/plugin-proposal-export-default-from',
-		'@babel/plugin-proposal-export-namespace-from',
-		'@babel/plugin-proposal-class-properties',
-		'@babel/plugin-proposal-object-rest-spread'
+        '@babel/plugin-transform-modules-commonjs',
+        ['@babel/preset-env', {targets: {node: 'current'}}],
 	]
 };
