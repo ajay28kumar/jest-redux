a519c6e71abc016d0f8773b31be6f7a75a2e2028
Ajay Kumar <ajaykumar@Ajays-MacBook-Pro.local>
2019-10-26 17:10:55 +0530

    preact-redux

--- preact.config.js
+++ preact.config.js
@@ -2,25 +2,11 @@
 import envVars from 'preact-cli-plugin-env-vars';
 
 // these props are both optional'
-export default () => {
-	// process.env.PREACT_APP_ENV_NAME = process.env.PREACT_APP_ENV_NAME ? process.env.PREACT_APP_ENV_NAME : 'dev';
-	// envVars(config, env, helpers);
-	// config.output.publicPath = process.env.PREACT_APP_BASE_PATH;
-	// console.log('Environment : ' + process.env.PREACT_APP_ENV_NAME);
-	// console.log('Public path : ' + process.env.PREACT_APP_BASE_PATH);
-	// config.resolve = {
-	// 	alias: {
-	// 		// react: 'preact/compat',
-	// 		// 'react-dom': 'preact/compat'
-	// 	}
-	// };
-	// eslint-disable-next-line no-undef
-	return {
-		resolve: {
-			alias: {
-				react: 'preact/compat'
-			}
-		}
-	};
-
+export default  (config, env, helpers) => {
+	process.env.PREACT_APP_ENV_NAME = process.env.PREACT_APP_ENV_NAME ? process.env.PREACT_APP_ENV_NAME : 'dev';
+	envVars(config, env, helpers);
+	config.output.publicPath = process.env.PREACT_APP_BASE_PATH;
+	console.log('Environment : ' + process.env.PREACT_APP_ENV_NAME);
+	console.log('Public path : ' + process.env.PREACT_APP_BASE_PATH);
+	return config;
 };
